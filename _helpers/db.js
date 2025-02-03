d4b6bd4ed8ba2a78b2e144c7c5e3127cf6d20e55
xav <xav1234g@gmail.com>
2025-02-04 01:34:15 +0800

    ‎

---
+++ _helpers/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+    const { host, port, user, password, database } = process.env;
+    const connection = await mysql.createConnection({ host: host, port: port, user: user, password: password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    const sequelize = new Sequelize(database, user, password, {dialect: 'mysql', host: host, port: port, define: {id: false,}, logging: false }); 
+
+    db.User = require('../users/user.model')(sequelize);
+
+    await sequelize.sync({alter: true});
+}
