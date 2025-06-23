c75e77facb181d3c5f69d6b3aa3e235c8de50bfc
marc <xav1234g@gmail.com>
2025-06-23 13:40:16 +0800

    cbcvcv

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ require('rootpath')();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
 const errorHandler = require('_middleware/error-handler');
+
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cors());
 
-// app.use('/users', require('./users/users.controller'));
-// app.use('/login', require('./user-authentication/auth.controller'));
+app.get('/ping', (req, res) => {
+  console.log('Ping received');
+  res.status(200).send();  // Respond with no content (status 200)
+});
+
+setInterval(async () => {
+  try {
+    await axios.get(`http://localhost:${port}/ping`);
+    console.log('Self-ping successful');
+  } catch (error) {
+    console.error('Error with self-ping', error);
+  }
+}, 5000);  // 10 minutes in milliseconds
+
 app.use('/quote', require('./quotation/quotation.controller'));
 
 app.use(errorHandler);
